fef54c22b2b30d6dfb85ee91bd45808201f24690
Punam Bagal <punambagal00@gmail.com>
2024-11-10 10:30:57 +0530

    task1 by Dev1

--- JavaProj2/src/test/java/com/nt/AppTest.java
+++ JavaProj2/src/test/java/com/nt/AppTest.java
@@ -1,19 +1,36 @@
 package com.nt;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testSumWithPositives() {
+    	
+    	App app=new App();
+    	int expected=300;
+    	int actual=app.sum(100, 200);
+    assertEquals(expected,actual);
+    }
+    @Test
+    public void testSumWithNegatives() {
+    	
+    	App app=new App();
+    	int expected=-300;
+    	int actual=app.sum(-100, -200);
+    assertEquals(expected,actual);
+    }
+    @Test
+    public void testSumWithMixedValues() {
+    	
+    	App app=new App();
+    	int expected=100;
+    	int actual=app.sum(-100, 200);
+    assertEquals(expected,actual);
     }
 }
